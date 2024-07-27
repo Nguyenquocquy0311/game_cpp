a443b65368aca1e7add204430e07d6ded5a0a245
Nguyen Quoc Quy <quocquynguyen0311@gmail.com>
2024-07-27 21:08:26 +0700

    v0.2:multi levels

--- main.cpp
+++ main.cpp
@@ -1,21 +1,24 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <conio.h> // For _kbhit() and _getch()
-#include <windows.h> // For Sleep()
+#include <conio.h>
+#include <windows.h>
 
 using namespace std;
 
 const int width = 20;
 const int height = 10;
-const char playerChar = '->';
+const char playerChar = '>';
 const char obstacleChar = '=';
 const char emptyChar = ' ';
 int playerPos = height / 2;
-vector<int> obstaclePos(width, -1); // Initialize obstacle positions
+vector<int> obstaclePos(width, -1);
+int level = 1;
+int obstacleSpeed = 200;
 
 void draw() {
-    system("cls"); // Clear the console screen
+    system("cls");
+    cout << "Level: " << level << endl;
     for (int y = 0; y < height; y++) {
         for (int x = 0; x < width; x++) {
             if (x == 0 && y == playerPos) {
@@ -31,12 +34,11 @@ void draw() {
 }
 
 void update() {
-    // Move obstacles left
     for (int x = 0; x < width - 1; x++) {
         obstaclePos[x] = obstaclePos[x + 1];
     }
     // Generate new obstacle
-    if (rand() % 3 == 0) { // Random chance of generating an obstacle
+    if (rand() % (3 - level / 3) == 0) {
         obstaclePos[width - 1] = rand() % height;
     } else {
         obstaclePos[width - 1] = -1;
@@ -48,6 +50,18 @@ bool checkCollision() {
         return true;
     }
     return false;
+}
+
+void nextLevel() {
+    level++;
+    obstacleSpeed -= 20;
+    if (obstacleSpeed < 50) {
+        obstacleSpeed = 50;
+    }
+    playerPos = height / 2;
+    obstaclePos = vector<int>(width, -1);
+    cout << "Next Level: " << level << "!" << endl;
+    Sleep(1000);
 }
 
 int main() {
@@ -66,7 +80,10 @@ int main() {
             cout << "Game Over!" << endl;
             break;
         }
-        //Sleep(200); // Slow down the loop
+        Sleep(obstacleSpeed);
+        if (rand() % 10 == 0) {
+            nextLevel();
+        }
     }
     return 0;
 }
